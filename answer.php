b13ab0569a70dd1c6401f8eaec36adae95ac74c0
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-21 00:20:53 +0000

    template

--- answer.php
+++ answer.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit2-03-HTML-MDL -->
+<!-- ICS2O-Unit1-08-HTML-MDL -->
 <html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="The Address With PHP" />
+  <meta name="description" content="The Address with PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Joyce Nkengbeza" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,44 +30,33 @@
     sizes="16x16"
     href="./favicon-16x16new.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The Address With PHP</title>
+  <title>The Address in PHP</title>
 </head>
-
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Adress With PHP</span>
+        <span class="mdl-layout-title">The Address With PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
         <img src="./images/images.jpeg" alt="address image" />
       </div>
-      <br />
       <div class="page-content-php">
-        <form action="answer.php" method="GET">
-          <p>Street Name</p>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" name="streetname">
-            <label class="mdl-textfield__label" for="streetname-input">Street Name here ...</label>
-          </div>
-          <br />
-          <p>Street Number</p>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="streetnumber">
-            <label class="mdl-textfield__label" for="streetnumber-input">Street Number here ...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
-          <!-- Accent-colored raised button with ripple -->
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            type="submit">
-            Enter
-          </button>
-        </form>
+        <div id="user-address">
+          <?php
+          $streetnumber = $_GET["streetnumber"];
+          $streetname = $_GET["streetname"];
+
+          echo "<p>Your address is: " . $streetnumber . ", " . $streetname . ".</p>";
+          ?>
+        </div>
+        <div class="page-content-answer">
+          <a href="./index.php">Return ...</a>
+        </div>
       </div>
     </main>
   </div>
