ba2fe1ff347f53f3d5668a77894c149518fdb5f0
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-21 00:37:39 +0000

    added in variables

--- answer.php
+++ answer.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit1-08-HTML-MDL -->
+<!-- ICS2O-Unit2-04-HTML-MDL -->
 <html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="The Address with PHP" />
+  <meta name="description" content="Area Of Triangle, PHP " />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Joyce Nkengbeza" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,7 +30,7 @@
     sizes="16x16"
     href="./favicon-16x16new.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The Address in PHP</title>
+  <title>Area Of Triangle In PHP</title>
 </head>
 
 <body>
@@ -38,7 +38,7 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Address With PHP</span>
+        <span class="mdl-layout-title">Area Of Trianle, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
@@ -46,12 +46,15 @@
         <img src="./images/images.jpeg" alt="address image" />
       </div>
       <div class="page-content-php">
-        <div id="user-address">
+        <div id="area-triangle">
           <?php
-          $streetnumber = $_GET["streetnumber"];
-          $streetname = $_GET["streetname"];
-
-          echo "<p>Your address is: " . $streetnumber . ", " . $streetname . ".</p>";
+          $baselength = $_GET["base-length"];
+          $heightlength = $_GET["height-length"];
+          
+          // process
+          $area = ($baselength * $heightlength) /2;
+          // output 
+          echo "The area of the Triangle is " . $area . " cm².";
           ?>
         </div>
         <div class="page-content-answer">
