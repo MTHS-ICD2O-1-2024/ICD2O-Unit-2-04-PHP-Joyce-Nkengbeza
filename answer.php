4c531d78244d9c0c0b8e95f4c5fdb6828b391c1e
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-21 00:58:36 +0000

    fixed spelling

--- answer.php
+++ answer.php
@@ -38,7 +38,7 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Area Of Trianle, PHP</span>
+        <span class="mdl-layout-title">Area Of Triangle, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
